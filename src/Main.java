e5e02d300b93c42164a9fc907ab53e840bf1517d
egor <vlad.filin_2000@mail.ru>
2025-06-22 15:00:46 +0300

    4 задание

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0 / 3.5;
-        var cat = 3.6 / 1.6;
-        var paper = 763789 / 7639;
-
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
     }
 }
