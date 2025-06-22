1da8c5fcae611e4eb2b2eb1009b00615f47de159
egor <vlad.filin_2000@mail.ru>
2025-06-22 15:09:43 +0300

    7 задание

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         var mass1Boxer = 78.2;
         var mass2Boxer = 82.7;
-        System.out.println("Общая масса 2 боксеров " + (mass1Boxer + mass2Boxer) + " кг");
-        System.out.println("Разница между массами 2 боксеров " + (mass2Boxer - mass1Boxer) + " кг");
+        System.out.println("Остаток от деления между массами 2 боксеров " + (mass2Boxer % mass1Boxer) + " кг");
     }
 }
