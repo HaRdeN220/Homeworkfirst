be930e1a555983e4f8cd398119fa326cefbecfb5
egor <vlad.filin_2000@mail.ru>
2025-06-22 15:13:24 +0300

    8.1 задание

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        var mass1Boxer = 78.2;
-        var mass2Boxer = 82.7;
-        System.out.println("Остаток от деления между массами 2 боксеров " + (mass2Boxer % mass1Boxer) + " кг");
+        var hours = 640;
+        var timePerson = 8;
+        System.out.println("Всего работников в компании - " + (hours / timePerson) + " человек");
     }
 }
